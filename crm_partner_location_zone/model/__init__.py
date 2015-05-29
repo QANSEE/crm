e0b4778cc8f074ee3c9e64b8ce61e8f79a8e17b5
Jacques-Etienne Baudoux <je@bcim.be>
2015-05-29 13:29:33 +0200

    [ADD] crm_partner_location_zone

---
+++ crm_partner_location_zone/model/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# License, author and contributors information in:
+# __openerp__.py file at the root folder of this module.
+#
+
+from . import zone
+from . import better_zip
+from . import partner
+from . import lead
